a323c8d2a2983dc124196f95d9c6230c6dcf6829
Janis Bürkle <bks@teamtechnik.com>
2023-10-19 15:55:27 +0200

    First Version

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace UNO_Spielprojekt
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
